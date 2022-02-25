f4b523406579a001324b4542428553e811f89239
Banner Samuelson <bannersamuelson@gmail.com>
2022-02-25 11:31:45 -0500

    dots

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -10,22 +10,14 @@ export default function Buttons() {
   return (
 
     <nav className="bg-transparent dotstyle dotstyle-fillup hidden md:block p-5 fixed top-72 right-0">
-      <ul className="m-4">
-        <span className="hovertext" data-hover="Home"><li class=" m-4 bg-slate-50 hover:bg-slate-400 p-1 border-1"></li></span>
-        <li class="m-4 bg-slate-50 hover:bg-slate-400" ></li>
-        <li class="m-4 bg-slate-50 hover:bg-slate-400"></li>
-        <li class="m-4 bg-slate-50 hover:bg-slate-400"></li>
+      <ul className="m-3">
+        <HashLink to="/#home" className="hovertext" data-hover="home">
+          <li class="m-3 bg-slate-50 hover:bg-slate-400 p-1 border-1"></li></HashLink>
+        <HashLink to="/#projects" className="hovertext" data-hover="projects">
+          <li class=" m-3 bg-slate-50 hover:bg-slate-400 p-1 border-1"></li></HashLink>
+        <HashLink to="/#contact" className="hovertext" data-hover="contact">
+          <li class=" m-3 bg-slate-50 hover:bg-slate-400 p-1 border-1"></li></HashLink>
       </ul>
     </nav>
-
-    // <ul>
-    //   <li class="current"><a href="#">Home</a></li>
-    //   <li><HashLink to="/#home" className="w-12 h-12 bg-red-400 rounded-full">Home</HashLink></li>
-    //   <li><HashLink to="/#projects" className="w-12 h-12 bg-red-400 rounded-full">Home</HashLink></li>
-    //   <li><HashLink to="/#contact" className="w-12 h-12 bg-red-400 rounded-full">Home</HashLink></li>
-    //   <li><HashLink to="/#home" className="w-12 h-12 bg-red-400 rounded-full">Home</HashLink></li>
-    //   <li><HashLink to="/#home" className="w-12 h-12 bg-red-400 rounded-full">Home</HashLink></li>
-    // </ul>
-
   )
 }
