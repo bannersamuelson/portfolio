045c495c5e60cf9a8304feadeaf0a834955a644b
Banner Samuelson <bannersamuelson@gmail.com>
2022-03-04 11:54:08 -0500

    commented in functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-// import Home from './components/Home/Home';
-// import Projects from './components/Projects/Projects';
-// import Contact from './components/Contact/Contact';
-// import Buttons from './components/Buttons/Buttons';
-// import About from './components/About/About';
-// import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Buttons from './components/Buttons/Buttons';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 import './App.css'
 import Loader from "./components/Loader/Loader";
 
