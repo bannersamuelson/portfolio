4f93261505c36f5f3ef41e55c3d3540306081144
Banner Samuelson <bannersamuelson@gmail.com>
2022-03-06 20:52:24 -0500

    icons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from './components/Footer/Footer';
 import Curve from "./components/Curve/Curve";
 import './App.css'
 import Loader from "./components/Loader/Loader";
-
+import Icons from "./components/Icons/Icons"
 function App() {
 
   const [loading, setLoading] = useState(true);
@@ -28,13 +28,13 @@ function App() {
           <Buttons />
           <Projects />
 
+          <Icons />
           <div className="grid grid-cols-1 lg:grid-cols-2">
             <About />
             <Contact />
           </div>
           <Curve />
           <Footer />
-
         </div>
       ) : (
         <Loader />
