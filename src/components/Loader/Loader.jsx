1995c36bea5a78fecf5304ce5a8896ccbb3decea
Banner Samuelson <bannersamuelson@gmail.com>
2022-08-29 09:51:28 -0400

    changed loader to black

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import './Loader.css'
 export default function Loader() {
   return (
     <div className="h-screen">
-      <div className="bg-blen h-screen items-center justify-center px-4 pt-24 over:opacity-100 ">
+      <div className="bg-black h-screen items-center justify-center px-4 pt-24 over:opacity-100 ">
         <div className="align-items">
           <div className="text-slate-200 grid justify-center text-big ">
             <div className="loadingB">B</div>
