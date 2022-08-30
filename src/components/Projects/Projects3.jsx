aa3f34242ee1a33685db6fe5a4577531b1a6a462
Banner Samuelson <bannersamuelson@gmail.com>
2022-08-30 02:37:00 -0400

    update project cards

--- src/components/Projects/Projects3.jsx
+++ src/components/Projects/Projects3.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import github from '../../images/github.png'
+import openArrow from '../../images/open_arrow.png'
 
 export default function Projects() {
 
   return (
-    <div class="p-8 bg-gradient-to-r from-black to-grey">
-      <div class="rounded p-8 bg-black border-2 border-yellow">
-        <div class="flex pb-6  items-center justify-between">
+    <div class="grid grid-cols-1 justify-center p-8 m-0 bg-gradient-to-r from-black to-grey">
+      <div class="w-96 rounded p-8 bg-red-500 border-2 border-yellow">
+        <div class="flex pb-6 flex-row-reverse items-center justify-between">
+          <div class="h-8 w-8 flex mr-4">
+            <img src={github} alt="github" />
+            <img src={openArrow} alt="go to external site" className="mr-4" />
+          </div>
           <div class="flex">
             <a class="inline-block mr-4" href="#">
               <img class="rounded-full max-w-none w-12 h-12" src="https://randomuser.me/api/portraits/men/35.jpg" />
